95834f1e6190ef815ecb61b1fa1496de2db028ad
MarcosMendes9389 <marcosmendes9389@gmail.com>
2018-01-27 10:39:52 -0200

    --

--- index2.js
+++ index2.js
@@ -1,12 +1,13 @@
 const CryptoJS = require("crypto-js") // o node acha ele sozinho
 
-var ALG = process.argv[2]
-if(ALG == 'MD5')
+//var ALG = process.argv[2]
+
+if(process.env.ALG == 'MD5')
 {
    console.log(ALG + ' : ' +CryptoJS.MD5(ALG).toString(CryptoJS.enc.Base64))
 }
 
-if(ALG == 'SHA256')
+if(process.env.ALG == 'SHA256')
 {
     console.log( ALG + ' : ' +CryptoJS.SHA256(ALG).toString(CryptoJS.enc.Base64))
 }
